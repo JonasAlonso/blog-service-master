8fe38e7548a0249c5f057a59e1803f09c9aca834
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-07 17:03:01 +0200

    Controladores vacios

---
+++ src/main/java/es/kairosds/blogservice/controller/AutorController.java
@@ -0,0 +1,10 @@
+package es.kairosds.blogservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+public class AutorController {
+
+}
