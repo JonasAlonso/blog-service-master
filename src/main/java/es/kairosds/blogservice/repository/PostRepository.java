5f2294e591f3f2385ef308b9b6f50e0c89dc7c67
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 14:26:58 +0200

    eliminar post

--- src/main/java/es/kairosds/blogservice/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package es.kairosds.blogservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.kairosds.blogservice.model.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
