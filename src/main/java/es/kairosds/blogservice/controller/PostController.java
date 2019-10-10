28d8cb4d82e6621eb8246f341e753d4eddc5fc99
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 12:27:56 +0200

    Metodos para añadir comentarios

--- src/main/java/es/kairosds/blogservice/controller/PostController.java
+++ src/main/java/es/kairosds/blogservice/controller/PostController.java
@@ -1,13 +1,19 @@
 package es.kairosds.blogservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.kairosds.blogservice.exception.ComentarioOfensivoException;
+import es.kairosds.blogservice.model.Comentario;
 import es.kairosds.blogservice.model.Post;
 import es.kairosds.blogservice.service.PostService;
 
@@ -22,6 +28,18 @@ public class PostController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Post crearPost(@RequestBody Post post) {
 		return postService.crearPost(post);
+	}
+	
+	@GetMapping("/")
+	public List<Post> obtenerTodosLosArticulos(){
+		return postService.obtenerTodos();
+	}
+	
+	@PostMapping("/{id}/comentario")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Post comentarUnPost(@PathVariable Long id, @RequestBody Comentario comentario) throws ComentarioOfensivoException {
+		return postService.comentarUnPost(id,comentario);
+		
 	}
 
 }
