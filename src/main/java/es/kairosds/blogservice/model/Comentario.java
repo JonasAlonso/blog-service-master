c1c6df348e025e87232353b2a01c614b9da406b1
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 10:55:22 +0200

    Entidades

--- src/main/java/es/kairosds/blogservice/model/Comentario.java
+++ src/main/java/es/kairosds/blogservice/model/Comentario.java
@@ -1,11 +1,18 @@
 package es.kairosds.blogservice.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,16 +26,22 @@ import lombok.ToString;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Comentario {
+public class Comentario implements Serializable {
 	
 	@Id
-	@GeneratedValue( strategy = GenerationType.AUTO)
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private Long comentarioId;
 	
 	private String comentarista;
 	
 	private String contenido;
 	
-	private LocalDate publicacion;
+	private final LocalDate publicacion = LocalDate.now();
+	
+	@JsonIgnore
+	@ManyToOne ( fetch = FetchType.LAZY,
+	cascade = CascadeType.ALL)
+	@JoinColumn(name = "postId", nullable = false)
+	private Post post;
 
 }
--- src/main/java/es/kairosds/blogservice/model/Post.java
+++ src/main/java/es/kairosds/blogservice/model/Post.java
@@ -1,9 +1,14 @@
 package es.kairosds.blogservice.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +25,7 @@ import lombok.ToString;
 public class Post {
 	
 	@Id
-	@GeneratedValue( strategy = GenerationType.AUTO)
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private Long postId;
 	
 	private Long autorId;
@@ -28,5 +33,10 @@ public class Post {
 	private String autor;
 	
 	private String content;
+	
+	@OneToMany(cascade = CascadeType.ALL,
+			fetch = FetchType.LAZY,
+			mappedBy = "post")
+	private List<Comentario> comentarios;
 
 }
