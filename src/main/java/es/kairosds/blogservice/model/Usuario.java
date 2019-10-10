aaabb43ad3920b27feedae2ad2c457c7af9f4bfe
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 16:35:07 +0200

    Clase usuario

--- src/main/java/es/kairosds/blogservice/model/Usuario.java
+++ src/main/java/es/kairosds/blogservice/model/Usuario.java
@@ -1,25 +1,16 @@
 package es.kairosds.blogservice.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import es.kairosds.blogservice.model.Post.PostBuilder;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+
 
 @Data
-@ToString
 @Entity
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Usuario {
 	
 	@Id
