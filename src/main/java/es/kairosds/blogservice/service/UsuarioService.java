588621d17268e8ad989e81d03b9de932b3c71f3d
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 12:51:40 +0200

    Usuario servicio

--- src/main/java/es/kairosds/blogservice/service/UsuarioService.java
+++ src/main/java/es/kairosds/blogservice/service/UsuarioService.java
@@ -1,15 +1,29 @@
 package es.kairosds.blogservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.kairosds.blogservice.model.Usuario;
+import es.kairosds.blogservice.repository.UsuarioRepository;
+
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UsuarioService {
 
-	public long insertOne(Usuario usuario) {
-		// TODO Auto-generated method stub
-		return 0;
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	// private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public long insertarUsuario(Usuario usuario) {
+		// usuario.setPswd(bCryptPasswordEncoder.encode(appUser.getPassword()));
+		return usuarioRepository.save(usuario).getIdUsuario();
 	}
+	
+    public void borrarUsuario(long id) {
+
+    	usuarioRepository.deleteById(id);
+    }
 
 }
