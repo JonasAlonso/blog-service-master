1ae6dc364cee008b0de20af71619fa086b36b85f
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 09:53:46 +0200

    Capa de logica de comentarios

--- src/main/java/es/kairosds/blogservice/service/ComentarioService.java
+++ src/main/java/es/kairosds/blogservice/service/ComentarioService.java
@@ -1,8 +1,19 @@
 package es.kairosds.blogservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.kairosds.blogservice.model.Comentario;
+import es.kairosds.blogservice.repository.ComentarioRepository;
 
 @Service
 public class ComentarioService {
+	
+	@Autowired
+	ComentarioRepository comentarioRepository;
+	
+	public void comentar(Comentario comentario){
+		comentarioRepository.save(comentario);
+	}
 
 }
