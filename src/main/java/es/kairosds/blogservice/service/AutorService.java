4239afd1be6c8bb8a6c0364add1632d1eaee399b
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 09:51:51 +0200

    Capa de negocio de autores

--- src/main/java/es/kairosds/blogservice/service/AutorService.java
+++ src/main/java/es/kairosds/blogservice/service/AutorService.java
@@ -1,8 +1,19 @@
 package es.kairosds.blogservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.kairosds.blogservice.model.Autor;
+import es.kairosds.blogservice.repository.AutorRepository;
 
 @Service
 public class AutorService {
+	
+	@Autowired
+	AutorRepository autorRepository;
+	
+	public Autor guardarAutor(Autor autor) {
+		return autorRepository.save(autor);
+	}
 
 }
