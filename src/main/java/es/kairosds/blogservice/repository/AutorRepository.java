0bfcc039c8414a847bb55f0560bebf67072fc714
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 12:31:31 +0200

    Eliminar entidad autor

--- src/main/java/es/kairosds/blogservice/repository/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package es.kairosds.blogservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.kairosds.blogservice.model.Autor;
-
-public interface AutorRepository extends JpaRepository<Autor, Long> {
-
-}
