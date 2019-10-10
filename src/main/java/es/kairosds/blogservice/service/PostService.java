27858c0a36804295f8e3b8fd391d848b05871f95
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-10 09:54:37 +0200

    Capa de logica de servicio

--- src/main/java/es/kairosds/blogservice/service/PostService.java
+++ src/main/java/es/kairosds/blogservice/service/PostService.java
@@ -1,8 +1,19 @@
 package es.kairosds.blogservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.kairosds.blogservice.model.Post;
+import es.kairosds.blogservice.repository.PostRepository;
 
 @Service
 public class PostService {
+	
+	@Autowired
+	PostRepository postRepository;
+
+	public Post crearPost(Post post) {
+		return postRepository.save(post);
+	}
 
 }
