aa3539ea6e3ceafa7a1e293817cdaccb93363b14
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-17 10:01:03 +0200

    Corregir respuesta

--- src/main/java/es/kairosds/blogservice/service/AnalizadorDeLenguajeService.java
+++ src/main/java/es/kairosds/blogservice/service/AnalizadorDeLenguajeService.java
@@ -1,19 +1,19 @@
 package es.kairosds.blogservice.service;
 
-import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import es.kairosds.blogservice.model.RespuestaAnalisis;
 import es.kairosds.blogservice.model.Texto;
 
 @FeignClient(name = "analizador-de-texto", url = "${analizador.microservicio.url}")
 public interface AnalizadorDeLenguajeService {
 	
 	@RequestMapping( method = RequestMethod.POST, value = "/analizador/")
-	List<String> analizarComentario(@RequestBody Texto texto);
+	ResponseEntity<RespuestaAnalisis> analizarComentario(@RequestBody Texto texto);
 
 	
 }
