0693bb56e8433b976383976fc0090f0f06052754
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-18 13:42:24 +0200

    Configuracion de seguridad

--- src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
+++ src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package es.kairosds.blogservice.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
@@ -12,23 +11,11 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/**").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/**").permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/").permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/").permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/{id}/").permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/").permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/comentario").permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/comentario/").permitAll();
-
-	}
-	
-	@Override
-	public void configure(WebSecurity webSecurity) throws Exception
-	{
-	    webSecurity
-	        .ignoring()
-	            // All of Spring Security will ignore the requests
-	            .antMatchers("/articulos/**"); // APIs use a key
+		http.
+		//Pages with required authentication 
+				authorizeRequests().antMatchers(HttpMethod.POST, "/articulos/").authenticated().and().
+		//Pages without any authentication required		
+				authorizeRequests().antMatchers("/articulos/**").permitAll().and().
+        csrf().disable();
 	}
 }
