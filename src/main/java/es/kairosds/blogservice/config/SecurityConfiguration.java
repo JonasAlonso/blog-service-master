55c9260edd5991f2df640ff404acac00d30263c4
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-18 12:52:10 +0200

    Seguridad, no permite hacer post

--- src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
+++ src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
@@ -1,13 +1,23 @@
 package es.kairosds.blogservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @Configuration
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
 
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/{id}/").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/**").permitAll();
+		http.authorizeRequests().antMatchers("/articulos/{id}/comentario").permitAll();
+		http.authorizeRequests().antMatchers("/articulos/{id}/comentario/").permitAll();
+
 	}
 }
