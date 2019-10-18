4067de478a5f9c83cd82b98cc8abadf332fb3239
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-18 13:15:50 +0200

    configure(WebSecurity webSecurity) allows POST methods

--- src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
+++ src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
@@ -3,21 +3,32 @@ package es.kairosds.blogservice.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
 
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.POST,"/**").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/").permitAll();
 		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/**").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/{id}/").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/**").permitAll();
-		http.authorizeRequests().antMatchers("/articulos/{id}/comentario").permitAll();
-		http.authorizeRequests().antMatchers("/articulos/{id}/comentario/").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/**").permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/").permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/").permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.GET,"/articulos/{id}/").permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/").permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/comentario").permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.POST,"/articulos/{id}/comentario/").permitAll();
 
+	}
+	
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception
+	{
+	    webSecurity
+	        .ignoring()
+	            // All of Spring Security will ignore the requests
+	            .antMatchers("/articulos/**"); // APIs use a key
 	}
 }
