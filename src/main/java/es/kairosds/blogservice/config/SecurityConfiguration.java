ed9b7beb49b0a66b88f649a793f8e21fcfa11453
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2019-10-23 16:00:27 +0200

    add security configuration

--- src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
+++ src/main/java/es/kairosds/blogservice/config/SecurityConfiguration.java
@@ -1,28 +1,63 @@
 package es.kairosds.blogservice.config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import es.kairosds.blogservice.security.JWTAuthenticationFilter;
+import es.kairosds.blogservice.security.JWTAuthorizationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
 	
+	public static final String LOGIN_URL = "/login";
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+		   .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		   .cors().and()	
+           .csrf().disable().
 		//Pages with required authentication 
 				authorizeRequests().antMatchers(HttpMethod.POST, "/articulos/").authenticated().and().
+				authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL).permitAll().and().
 		//Pages without any authentication required		
-				authorizeRequests().antMatchers("/articulos/**").permitAll().and().
-        csrf().disable();
+				authorizeRequests().antMatchers("/articulos/**").permitAll().and()
+		.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+		.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
 	}
 }
